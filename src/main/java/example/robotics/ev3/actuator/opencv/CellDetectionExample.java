6e0b71080562e8e0b8d264f256702d76ab312d74
Sven Hakvoort <sven.hakvoort@gmail.com>
2022-12-01 16:40:07 +0100

    OpenCV tests

---
+++ src/main/java/example/robotics/ev3/actuator/opencv/CellDetectionExample.java
@@ -0,0 +1,70 @@
+package example.robotics.ev3.actuator.opencv;
+
+import nu.pattern.OpenCV;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+
+import static example.robotics.ev3.actuator.opencv.ImageUtils.matToBufferedImage;
+
+public class CellDetectionExample {
+
+    public static Logger LOGGER = LoggerFactory.getLogger(CellDetectionExample.class);
+
+    private BufferedImage image;
+    private Mat imageMat;
+    private ImageViewer viewer;
+
+    public CellDetectionExample() {
+        imageMat = Imgcodecs.imread("D:\\Users\\Sven\\Documents\\prgrm\\template-project-gradle\\src\\main\\resources\\cells.png");
+
+        Mat grayScaled = new Mat();
+        Imgproc.cvtColor(imageMat, grayScaled, Imgproc.COLOR_BGR2GRAY);
+
+        Mat floodfilled = Mat.zeros(imageMat.rows() + 2, imageMat.cols() + 2, CvType.CV_8UC1);
+        Point seed = new Point(imageMat.height()/2, imageMat.width()/2);
+        Imgproc.floodFill(grayScaled, floodfilled, seed, new Scalar(0, 0, 0), new Rect(), new Scalar(1), new Scalar(1));
+
+        Rect roi = new Rect(1, 1, grayScaled.cols() - 2, grayScaled.rows() - 2);
+        grayScaled = grayScaled.submat(roi);
+
+//        Mat blur = new Mat();
+//        Imgproc.GaussianBlur(grayScaled, blur, new Size(5,5), 0);
+//
+//        Mat canny = new Mat();
+//        Imgproc.Canny(blur, canny, 30, 150, 3);
+
+//        Mat dilated = new Mat();
+//        Mat kernel = Mat.ones(new Size(1, 1), CvType.CV_8U);
+//        Imgproc.dilate(canny, dilated, kernel, new Point(0,0), 0);
+
+        List<MatOfPoint> contours = new ArrayList<>();
+        Imgproc.findContours(grayScaled, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+        System.out.println(contours.size());
+
+        Imgproc.drawContours(imageMat, contours, -1, new Scalar(0, 255, 0), 2);
+        image = matToBufferedImage(imageMat);
+
+        if (viewer == null) {
+            viewer = new ImageViewer(image);
+        } else {
+            viewer.repaint(image);
+        }
+    }
+
+    public static void main(final String[] args) throws Exception {
+        OpenCV.loadShared();
+
+        CellDetectionExample example = new CellDetectionExample();
+
+        while (true) {
+        }
+    }
+
+}
---
+++ src/main/java/example/robotics/ev3/actuator/opencv/OpenCVMotionDetection.java
@@ -0,0 +1,114 @@
+package example.robotics.ev3.actuator.opencv;
+
+import nu.pattern.OpenCV;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.videoio.VideoCapture;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import static example.robotics.ev3.actuator.opencv.ImageUtils.matToBufferedImage;
+
+public class OpenCVMotionDetection {
+
+    public static Logger LOGGER = LoggerFactory.getLogger(OpenCVMotionDetection.class);
+
+    private BufferedImage image;
+    private Mat imageMat;
+    private Mat diffFrame = null;
+    private Mat tempFrame = null;
+    private boolean firstIteration = true;
+
+    private ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+    private VideoCapture camera;
+    private ImageViewer viewer;
+
+    public OpenCVMotionDetection() {
+        camera = new VideoCapture(0);
+        imageMat = new Mat();
+
+        Runnable frameGrabber = () -> {
+            try {
+                if (camera.read(imageMat)) {
+                    Mat inputClone = imageMat.clone();
+                    Mat outerBox = new Mat(inputClone.size(), CvType.CV_8UC1);
+                    Imgproc.cvtColor(inputClone, outerBox, Imgproc.COLOR_BGR2GRAY);
+                    Imgproc.GaussianBlur(outerBox, outerBox, new Size(3, 3), 0);
+
+                    if (firstIteration) {
+                        tempFrame = new Mat(outerBox.size(), CvType.CV_8UC1);
+                        diffFrame = outerBox.clone();
+                        firstIteration = false;
+                    } else {
+                        Core.absdiff(outerBox, tempFrame, diffFrame);
+
+                        Imgproc.threshold(diffFrame, diffFrame, 20, 255, Imgproc.THRESH_BINARY);
+
+                        Mat kernel2 = Mat.ones(new Size(imageMat.width()/100, imageMat.height()/100), CvType.CV_8U);
+                        Imgproc.dilate(diffFrame, diffFrame, kernel2, new Point(0,0), 1);
+
+                        List<Rect> contours = detectContours(diffFrame);
+//                        Imgproc.drawContours(imageMat, contours, -1, new Scalar(0, 255, 0), 2, Imgproc.LINE_AA);
+                        for (Rect contour : contours) {
+                            Imgproc.rectangle(imageMat, contour.br(), contour.tl(),
+                                    new Scalar(0, 255, 0), 1);
+                        }
+                    }
+
+                    image = matToBufferedImage(imageMat);
+
+                    if (viewer == null) {
+                        viewer = new ImageViewer(image);
+                    } else {
+                        viewer.repaint(image);
+                    }
+                    tempFrame = outerBox.clone();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+
+        this.executorService.scheduleAtFixedRate(frameGrabber, 0, 1000/30, TimeUnit.MILLISECONDS);
+    }
+
+    public static List<Rect> detectContours(Mat inputFrame) {
+        Mat hierarchy = new Mat();
+        Mat inputFrameClone = inputFrame.clone();
+        List<MatOfPoint> contours = new ArrayList<>();
+        Imgproc.findContours(inputFrameClone, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        double maxArea = 500;
+        List<Rect> rectList = new ArrayList<>();
+
+        for (Mat contour : contours) {
+            double contourarea = Imgproc.contourArea(contour);
+            if (contourarea > maxArea) {
+                Rect r = Imgproc.boundingRect(contour);
+                rectList.add(r);
+            }
+        }
+
+        hierarchy.release();
+
+        return rectList;
+    }
+
+    public static void main(final String[] args) throws Exception {
+        OpenCV.loadShared();
+
+        OpenCVMotionDetection example = new OpenCVMotionDetection();
+
+        while (true) {
+        }
+    }
+
+
+}
